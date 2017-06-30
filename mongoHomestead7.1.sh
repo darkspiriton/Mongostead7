fcca65a17ae288c2d6b3f1b66aeae3f44aef45f1
Saud <alquesadilla@users.noreply.github.com>
2017-06-30 12:20:08 -0500

    mongodb 3.4

--- mongoHomestead7.1.sh
+++ mongoHomestead7.1.sh
@@ -2,10 +2,10 @@ echo "MongoDB install  script with PHP7 & nginx [Laravel Homestead]"
 echo "By Zakaria BenBakkar, @zakhttp, zakhttp@gmail.com"
 
 echo "Importing the public key used by the package management system";
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927;
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6;
 
 echo "Creating a list file for MongoDB.";
-echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list;
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list;
 
 echo "Updating the packages list";
 sudo apt-get update;
