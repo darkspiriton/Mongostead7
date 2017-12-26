c026a00d9d639d7037d739efff246518c118ccb4
Saud <alquesadilla@users.noreply.github.com>
2017-12-26 09:26:02 -0600

    Update mongoHomestead7.1.sh

--- mongoHomestead7.1.sh
+++ mongoHomestead7.1.sh
@@ -5,7 +5,7 @@ echo "Importing the public key used by the package management system";
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6;
 
 echo "Creating a list file for MongoDB.";
-echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list;
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list;
 
 echo "Updating the packages list";
 sudo apt-get update;
