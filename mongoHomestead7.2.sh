f52fd4b2b3529a413f6bf93fa1fccb8aba592f47
Moreno Cesar <cesar200526@gmail.com>
2018-11-05 14:28:44 -0500

    update version mongo

--- mongoHomestead7.2.sh
+++ mongoHomestead7.2.sh
@@ -1,11 +1,12 @@
 echo "MongoDB install  script with PHP7 & nginx [Laravel Homestead]"
 echo "By Zakaria BenBakkar, @zakhttp, zakhttp@gmail.com"
+echo "Updated php7.2 & mongo4.0 by Cesar Moreno morenodev@gmail.com"
 
 echo "Importing the public key used by the package management system";
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6;
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4;
 
 echo "Creating a list file for MongoDB.";
-echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list;
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 
 echo "Updating the packages list";
 sudo apt-get update;
@@ -46,6 +47,9 @@ EOL
 
 # Change mongoDB Listening IP Address from local 127.0.0.1 to All IPs 0.0.0.0
 sudo sed -i 's/127\.0\.0\.1/0\.0\.0\.0/g' /etc/mongod.conf
+
+# Change mongoDB Listening PORT from 27017 to 37037
+sudo sed -i 's/27017/37037/g' /etc/mongod.conf
 
 sudo systemctl start mongodb
 sudo systemctl status mongodb
